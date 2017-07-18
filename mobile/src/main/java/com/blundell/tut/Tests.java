32756e7ec5fa3df8312d1194b1c0df74f22d6a38
Blundell <paul@novoda.com>
2017-07-18 10:53:02 +0100

    more colors

--- mobile/src/main/java/com/blundell/tut/Tests.java
+++ mobile/src/main/java/com/blundell/tut/Tests.java
@@ -12,39 +12,39 @@ public class Tests {
         }
     }
 
-    public static void drawThreeSquares(RGBmatrixPanelNoObjects rgbMatrixPanel) {
+    public static void drawThreeSquares(RGBmatrixPanel rgbMatrixPanel) {
         // A square
         rgbMatrixPanel.drawPixel(3, 3, Color.YELLOW);
-        rgbMatrixPanel.drawPixel(4, 3, Color.YELLOW);
+        rgbMatrixPanel.drawPixel(4, 3, Color.RED);
         rgbMatrixPanel.drawPixel(5, 3, Color.YELLOW);
-        rgbMatrixPanel.drawPixel(6, 3, Color.YELLOW);
+        rgbMatrixPanel.drawPixel(6, 3, Color.RED);
         rgbMatrixPanel.drawPixel(7, 3, Color.YELLOW);
-        rgbMatrixPanel.drawPixel(8, 3, Color.YELLOW);
+        rgbMatrixPanel.drawPixel(8, 3, Color.RED);
         rgbMatrixPanel.drawPixel(9, 3, Color.YELLOW);
 
-        rgbMatrixPanel.drawPixel(3, 3, Color.YELLOW);
+        rgbMatrixPanel.drawPixel(3, 3, Color.RED);
         rgbMatrixPanel.drawPixel(3, 4, Color.YELLOW);
-        rgbMatrixPanel.drawPixel(3, 5, Color.YELLOW);
+        rgbMatrixPanel.drawPixel(3, 5, Color.RED);
         rgbMatrixPanel.drawPixel(3, 6, Color.YELLOW);
-        rgbMatrixPanel.drawPixel(3, 7, Color.YELLOW);
+        rgbMatrixPanel.drawPixel(3, 7, Color.RED);
         rgbMatrixPanel.drawPixel(3, 8, Color.YELLOW);
-        rgbMatrixPanel.drawPixel(3, 9, Color.YELLOW);
+        rgbMatrixPanel.drawPixel(3, 9, Color.RED);
 
         rgbMatrixPanel.drawPixel(9, 3, Color.YELLOW);
-        rgbMatrixPanel.drawPixel(9, 4, Color.YELLOW);
+        rgbMatrixPanel.drawPixel(9, 4, Color.RED);
         rgbMatrixPanel.drawPixel(9, 5, Color.YELLOW);
-        rgbMatrixPanel.drawPixel(9, 6, Color.YELLOW);
+        rgbMatrixPanel.drawPixel(9, 6, Color.RED);
         rgbMatrixPanel.drawPixel(9, 7, Color.YELLOW);
-        rgbMatrixPanel.drawPixel(9, 8, Color.YELLOW);
+        rgbMatrixPanel.drawPixel(9, 8, Color.RED);
         rgbMatrixPanel.drawPixel(9, 9, Color.YELLOW);
 
-        rgbMatrixPanel.drawPixel(3, 9, Color.YELLOW);
+        rgbMatrixPanel.drawPixel(3, 9, Color.RED);
         rgbMatrixPanel.drawPixel(4, 9, Color.YELLOW);
-        rgbMatrixPanel.drawPixel(5, 9, Color.YELLOW);
+        rgbMatrixPanel.drawPixel(5, 9, Color.RED);
         rgbMatrixPanel.drawPixel(6, 9, Color.YELLOW);
-        rgbMatrixPanel.drawPixel(7, 9, Color.YELLOW);
+        rgbMatrixPanel.drawPixel(7, 9, Color.RED);
         rgbMatrixPanel.drawPixel(8, 9, Color.YELLOW);
-        rgbMatrixPanel.drawPixel(9, 9, Color.YELLOW);
+        rgbMatrixPanel.drawPixel(9, 9, Color.RED);
 
         // A square
         rgbMatrixPanel.drawPixel(13, 13, Color.GREEN);
@@ -81,40 +81,40 @@ public class Tests {
 
         // A square
         rgbMatrixPanel.drawPixel(23, 23, Color.RED);
-        rgbMatrixPanel.drawPixel(24, 23, Color.RED);
-        rgbMatrixPanel.drawPixel(25, 23, Color.RED);
+        rgbMatrixPanel.drawPixel(24, 23, Color.GREEN);
+        rgbMatrixPanel.drawPixel(25, 23, Color.BLUE);
         rgbMatrixPanel.drawPixel(26, 23, Color.RED);
-        rgbMatrixPanel.drawPixel(27, 23, Color.RED);
-        rgbMatrixPanel.drawPixel(28, 23, Color.RED);
+        rgbMatrixPanel.drawPixel(27, 23, Color.GREEN);
+        rgbMatrixPanel.drawPixel(28, 23, Color.BLUE);
         rgbMatrixPanel.drawPixel(29, 23, Color.RED);
 
         rgbMatrixPanel.drawPixel(23, 23, Color.RED);
-        rgbMatrixPanel.drawPixel(23, 24, Color.RED);
-        rgbMatrixPanel.drawPixel(23, 25, Color.RED);
+        rgbMatrixPanel.drawPixel(23, 24, Color.GREEN);
+        rgbMatrixPanel.drawPixel(23, 25, Color.BLUE);
         rgbMatrixPanel.drawPixel(23, 26, Color.RED);
-        rgbMatrixPanel.drawPixel(23, 27, Color.RED);
-        rgbMatrixPanel.drawPixel(23, 28, Color.RED);
+        rgbMatrixPanel.drawPixel(23, 27, Color.GREEN);
+        rgbMatrixPanel.drawPixel(23, 28, Color.BLUE);
         rgbMatrixPanel.drawPixel(23, 29, Color.RED);
 
         rgbMatrixPanel.drawPixel(29, 23, Color.RED);
-        rgbMatrixPanel.drawPixel(29, 24, Color.RED);
-        rgbMatrixPanel.drawPixel(29, 25, Color.RED);
+        rgbMatrixPanel.drawPixel(29, 24, Color.GREEN);
+        rgbMatrixPanel.drawPixel(29, 25, Color.BLUE);
         rgbMatrixPanel.drawPixel(29, 26, Color.RED);
-        rgbMatrixPanel.drawPixel(29, 27, Color.RED);
-        rgbMatrixPanel.drawPixel(29, 28, Color.RED);
+        rgbMatrixPanel.drawPixel(29, 27, Color.GREEN);
+        rgbMatrixPanel.drawPixel(29, 28, Color.BLUE);
         rgbMatrixPanel.drawPixel(29, 29, Color.RED);
 
         rgbMatrixPanel.drawPixel(23, 29, Color.RED);
-        rgbMatrixPanel.drawPixel(24, 29, Color.RED);
-        rgbMatrixPanel.drawPixel(25, 29, Color.RED);
+        rgbMatrixPanel.drawPixel(24, 29, Color.GREEN);
+        rgbMatrixPanel.drawPixel(25, 29, Color.BLUE);
         rgbMatrixPanel.drawPixel(26, 29, Color.RED);
-        rgbMatrixPanel.drawPixel(27, 29, Color.RED);
-        rgbMatrixPanel.drawPixel(28, 29, Color.RED);
+        rgbMatrixPanel.drawPixel(27, 29, Color.GREEN);
+        rgbMatrixPanel.drawPixel(28, 29, Color.BLUE);
         rgbMatrixPanel.drawPixel(29, 29, Color.RED);
     }
 
     public static void writeHelloWorld(RGBmatrixPanelNoObjects rgbMatrixPanel) {
-        rgbMatrixPanel.setFontColor(Color.YELLOW);
+        rgbMatrixPanel.setFontColor(Color.BLUE);
         rgbMatrixPanel.writeText("Hello World");
     }
 }
